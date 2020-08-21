aac8db688eecb8f26bf5d2c6ef7d0c58d581a812
Merunas Grincalaitis <merunasgrincalaitis@gmail.com>
2020-08-21 11:28:01 +0200

    Created automation to scrape articles

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,4 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -28,7 +25,7 @@ class MerunasDriver {
         prefs.put("profile.default_content_setting_values.notifications", 2);
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("prefs", prefs);
-        System.setProperty("webdriver.chrome.driver", "/Users/merunas/Documents/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\merun\\Documents\\chromedriver\\chromedriver.exe");
         driver = new ChromeDriver(options);
 
         mediumArticleExtractor();
@@ -37,23 +34,72 @@ class MerunasDriver {
     void mediumArticleExtractor() throws InterruptedException, IOException {
         driver.get("https://medium.com/@samajammin/how-to-interact-with-ethereums-mainnet-in-a-development-environment-with-ganache-3d8649df0876");
         try {
-            Robot robot = new Robot();
-            robot.mouseMove(500, 200);
-            WebElement img = driver.findElement(By.cssSelector("img"));
-            String logoSrc = img.getAttribute("src");
-            driver.get(logoSrc);
+            // 1 Extract title
+            WebElement titleElement = driver.findElement(By.cssSelector("h1"));
+            String title = titleElement.getText();
+            System.out.println("Title " + title);
             Thread.sleep(1000);
 
-            robot.keyPress(KeyEvent.VK_META);
-            robot.keyPress(KeyEvent.VK_S);
-            robot.keyRelease(KeyEvent.VK_S);
-            robot.keyRelease(KeyEvent.VK_META);
-            Thread.sleep(10000);
-        } catch (AWTException e) {
-            e.printStackTrace();
+            // 2 Extract subtitle
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            String subtitle = js.executeScript("return document.querySelector('h1').parentNode.parentNode.querySelector('p').textContent").toString();
+            System.out.println("Subtitle " + subtitle);
+            Thread.sleep(1000);
+
+            // 3 Resize images
+            System.out.println("Resizing images...");
+            String resizeImagesScript = String.join("\n",
+                "document.querySelectorAll('noscript').forEach(noscript => noscript.remove())",
+                    "document.querySelectorAll('img').forEach(img => {",
+                        "if (!img.hasAttribute('src') || img.hasAttribute('srcSet') || img.hasAttribute('srcset') || img.hasAttribute('sizes')) {",
+                            "img.remove()",
+                        "} else {",
+                            "img.src = img.src.replace(/(https:\\/\\/miro\\.medium\\.com\\/max\\/).*\\/(.*)/, '$11280/$2').replace('?q=20', '')",
+                            "img.removeAttribute('width')",
+                            "img.removeAttribute('height')",
+                        "}",
+                    "})",
+                    "window.scrollTo(0, 0)"
+                );
+            js.executeScript(resizeImagesScript);
+            System.out.println("Image resizing complete");
+            Thread.sleep(1000);
+
+            // 4 Extract content
+            System.out.println("Extracting content...");
+            WebElement container = driver.findElement(By.xpath("//p/../.."));
+            System.out.println("Container content -> " + container.getAttribute("innerHTML"));
+            String contentHTML = container.getAttribute("innerHTML");
+            String filteredContentHTML = js.executeScript("return arguments[0].replace(/(<div.+?>)(.*?)(<p )/, '$1$3')", contentHTML).toString();
+            System.out.println("Filtered -> " + filteredContentHTML);
+            System.out.println("Done extracting article content");
+
+            // 5 Save image
+            System.out.println("Saving image...");
+            List<WebElement> imgs = driver.findElements(By.cssSelector("img"));
+            String logoSrc = imgs.get(4).getAttribute("src");
+            saveImage(logoSrc);
+            System.out.println("Image saving complete");
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            System.out.println("Exception " + e);
         } finally {
             System.out.println("Done");
             driver.close();
+        }
+    }
+
+    void saveImage(String url) throws InterruptedException, IOException {
+        try {
+            driver.get(url);
+            Robot robot = new Robot();
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_S);
+            robot.keyRelease(KeyEvent.VK_S);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            Thread.sleep(10000);
+        } catch (AWTException e) {
+            e.printStackTrace();
         }
     }
 
